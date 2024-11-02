e630ad506cbd97f0a257faef224c65d6616d4820
filippodanesi <filippo.danesi93@gmail.com>
2024-11-02 15:16:45 +0100

    s

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -66,19 +66,13 @@ const siteConfig: SiteConfig = {
         {
             text: 'About',
             href: '/about'
-        }
-    ],
-    footerNavLinks: [
-        {
-            text: 'About',
-            href: '/about'
         },
         {
             text: 'Contact',
             href: '/contact'
         }
     ],
-    socialLinks: [
+    footerNavLinks: [
         {
             text: 'LinkedIn',
             href: 'https://www.linkedin.com/in/filippodanesi/'
@@ -92,19 +86,9 @@ const siteConfig: SiteConfig = {
             href: 'https://x.com/filippodanesi'
         }
     ],
+    socialLinks: [],
     hero: {
-        title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
-        image: {
-            src: '/about.jpeg',
-            alt: 'A human with retrofuturistic gear in retro neon gradient'
-        },
-        actions: [
-            {
-                text: 'Get in Touch',
-                href: '/contact'
-            }
-        ]
+        actions: []
     },
     subscribe: {
         title: 'Join the SERPsecrets Newsletter',
