76eec79eb30470c50ee5e5a4faeaf5a618065f94
filippodanesi <filippo.danesi93@gmail.com>
2024-11-06 15:07:19 +0100

    sa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel/serverless';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import partytown from '@astrojs/partytown';
