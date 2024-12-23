ec9ce812afd7c19b6e9260483755627720e5434b
Filippo Danesi <50838949+filippodanesi@users.noreply.github.com>
2024-12-23 21:34:02 +0100

    sa

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -88,7 +88,7 @@ const siteConfig: SiteConfig = {
     ],
 hero: {
     title: 'Filippo Danesi',
-    text: `> SEO is my passion, driven by data and innovation.
+    text: `> _SEO is my passion, driven by data and innovation._
 
 My journey in SEO began with a fascination for data and a desire to bridge the gap between technology and user experience. What started as curiosity evolved into a five-year adventure across different markets and challenges, each one teaching me something new about the ever-evolving world of search.
 
