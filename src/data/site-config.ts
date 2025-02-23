f2ed0caf5825a3e68c1ff01be1b823fe431d2853
Filippo Danesi <50838949+filippodanesi@users.noreply.github.com>
2025-02-23 10:16:51 +0100

    sa

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -55,7 +55,7 @@ const siteConfig: SiteConfig = {
         },
         {
             text: 'Contact',
-            href: '/contact'
+            href: '/contact/'
         }
     ],
     footerNavLinks: [
