fdc204f44bc81116991730c55be51dc83107930f
Filippo Danesi <50838949+filippodanesi@users.noreply.github.com>
2025-02-23 10:19:10 +0100

    sa

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import rehypeExternalLinks from 'rehype-external-links';
 
 export default defineConfig({
   site: 'https://www.filippodanesi.it',
@@ -10,6 +11,17 @@ export default defineConfig({
     isr: { expiration: 60 * 60 * 24 },
   }),
   trailingSlash: 'always',
+  markdown: {
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          rel: ['nofollow', 'noopener', 'noreferrer'],
+          target: '_blank'
+        }
+      ]
+    ]
+  },
   integrations: [
     sitemap(),
     tailwind({ applyBaseStyles: false }),
